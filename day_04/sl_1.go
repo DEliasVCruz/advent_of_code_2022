a213726e594cbe84d7ab038f35782079064d29a7
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-05 09:03:12 -0500

    feat: add solution for problem first part day4

---
+++ day_04/sl_1.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type void struct{}
+
+var member void
+
+func getSet(array []string) map[string]void {
+	set := make(map[string]void)
+	for _, value := range array {
+		set[value] = member
+	}
+
+	return set
+}
+
+func strToInt(value string) int {
+	convertedStr, err := strconv.Atoi(value)
+	check(err)
+	return convertedStr
+}
+
+func main() {
+	input, err := os.Open("./input.txt")
+	check(err)
+	defer input.Close()
+
+	var fullyContainedRangeCounter = 0
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		rangePairs := strings.Split(scanner.Text(), ",")
+		firstPair := rangePairs[0]
+		secondPair := rangePairs[1]
+
+		firstPairRange := strings.Split(firstPair, "-")
+		secondPairRange := strings.Split(secondPair, "-")
+
+		firstPairRangeLower := strToInt(firstPairRange[0])
+		secondPairRangeLower := strToInt(secondPairRange[0])
+		firstPairRangeUpper := strToInt(firstPairRange[1])
+		secondPairRangeUpper := strToInt(secondPairRange[1])
+
+		if firstPairRangeLower > secondPairRangeLower {
+			if firstPairRangeUpper <= secondPairRangeUpper {
+				fullyContainedRangeCounter += 1
+			}
+		} else if firstPairRangeLower < secondPairRangeLower {
+			if firstPairRangeUpper >= secondPairRangeUpper {
+				fullyContainedRangeCounter += 1
+			}
+		} else {
+			fullyContainedRangeCounter += 1
+		}
+
+	}
+	check(scanner.Err())
+
+	fmt.Println(fullyContainedRangeCounter)
+}
