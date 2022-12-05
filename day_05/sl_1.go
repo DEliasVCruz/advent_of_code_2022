2092c0c372ff2248b030a9fb2cea0dd960115eb8
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-05 11:29:30 -0500

    feat: add solution for problem first part day5

---
+++ day_05/sl_1.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type void struct{}
+
+var member void
+
+func getSet(array []string) map[string]void {
+	set := make(map[string]void)
+	for _, value := range array {
+		set[value] = member
+	}
+
+	return set
+}
+
+func strToInt(value string) int {
+	convertedStr, err := strconv.Atoi(value)
+	check(err)
+	return convertedStr
+}
+
+func reverseArray(characters []string) {
+	for i, j := 0, len(characters)-1; i < j; i, j = i+1, j-1 {
+		characters[i], characters[j] = characters[j], characters[i]
+	}
+}
+
+func main() {
+	input, err := os.Open("./input.txt")
+	check(err)
+	defer input.Close()
+
+	crateSectionCounter := 1
+	stacks := make(map[int][]string)
+
+	var instructions [3]int
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		inputLine := scanner.Text()
+
+		if crateSectionCounter < 9 {
+			for indx, char := range inputLine {
+				trueIndex := indx - 1
+				if ((trueIndex)%4) == 0 && string(char) != " " {
+					stackNumber := trueIndex/4 + 1
+					stacks[stackNumber] = append(stacks[stackNumber], string(char))
+				}
+			}
+			crateSectionCounter++
+			continue
+		} else if crateSectionCounter < 11 {
+			crateSectionCounter++
+			continue
+		} else if crateSectionCounter == 11 {
+			for key := range stacks {
+				reverseArray(stacks[key])
+			}
+			crateSectionCounter++
+		}
+
+		regex, _ := regexp.Compile(`\d+`)
+		matches := regex.FindAllString(inputLine, -1)
+
+		for indx, value := range matches {
+			instructions[indx] = strToInt(value)
+		}
+
+		for i := 0; i < instructions[0]; i++ {
+			fromStackIndex := instructions[1]
+			toStackIndex := instructions[2]
+
+			lastIndex := len(stacks[fromStackIndex]) - 1
+			crate := stacks[fromStackIndex][lastIndex]
+
+			stacks[fromStackIndex] = stacks[fromStackIndex][:lastIndex]
+			stacks[toStackIndex] = append(stacks[toStackIndex], crate)
+		}
+
+	}
+	check(scanner.Err())
+
+	var stringSlice [9]string
+
+	for i := 0; i < 9; i++ {
+		stringSlice[i] = stacks[i+1][len(stacks[i+1])-1]
+	}
+
+	fmt.Println(strings.Join(stringSlice[:], ""))
+}
